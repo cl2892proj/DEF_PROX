4abd8bfaae39235b0f24b1e6a7d771d5d1242ba4
junkseeker <pixiu521@gmail.com>
2016-07-12 23:34:03 -0400

    parse by colspan, got stuck by a table with strange format. the table combines name and positions in one column

--- def_14a.py
+++ def_14a.py
@@ -86,6 +86,89 @@ def combine_similar_cells(arr, x):
     return result
 
 
+def valid_table_inputs(tbl):
+    #check if every row has the same number of columns
+    col_per_row = map(lambda x: len(x), tbl)
+    n = len(set(col_per_row))
+    return n == 1
+
+
+
+def remove_empty_rows(tbl):
+    # if a row only has '' then remove the row
+    result = []
+    for row in tbl:
+        s = set(row)
+        if len(s) > 1:
+            result.append(row)
+        if len(s) == 1 and ('' not in s):
+            result.append(row)
+    return result
+
+
+def remove_empty_string(row): 
+    #remove elements that == '' from a list of strings
+    return filter(lambda x: x not in ('', '$'), row)
+
+def remove_empty_cells(tbl):
+    result = []
+    # assume the header rows have the same number of elements
+    # then the data rows have different number of elements
+    col_per_row = map(lambda x: len(x), tbl)
+    n = next((i for i,v in enumerate(col_per_row) if v!=col_per_row[0]),-1)
+    arr = np.array(tbl[:n])
+    h_arr = np.transpose(arr)
+    header = map(lambda x: x.strip(), [' '.join(row) for row in h_arr])
+    result.append(remove_empty_string(header))
+    for row in tbl[n:]:
+        result.append(remove_empty_string(row))
+
+    col_per_row = map(lambda x: len(x), result)
+
+    pdb.set_trace()
+    if len(set(col_per_row)) == 1:
+        return result
+    else:
+        return tbl
+
+def rebuild_table_by_colspan(table_soup):
+    #content per colspan
+    temp = []
+    s = set()
+    for tr in table_soup.find_all('tr'):
+        row = []
+        offset = 0
+        for td in tr.find_all('td'):
+            try:
+                td_col = int(td['colspan'])
+            except:
+                td_col = 1
+
+            offset += td_col
+            txt = clean_tag_text(td.get_text())
+            if txt != '':
+                s.add(offset)
+            row.append((offset, txt))
+        temp.append(row)
+
+    result = []
+    for row in temp:
+        i = []
+        for item in row: 
+            if item[0] in s:
+                i.append(item[1])
+        result.append(i)
+
+    return result
+
+def clean_tag_text(txt):
+    s = removeNonAscii(txt)
+    #remove white spaces
+    s = ' '.join(s.split())
+    s = s.strip()
+    return s
+
+
 def download_proxy(index): 
     proxy = index[0]
     fn = '{0}/cik_{1}_date_{2}_orig.html'.format(DATA_DIR, proxy['cik'], proxy['date'])
@@ -102,50 +185,70 @@ def download_proxy(index):
     result = [] 
     fn = '{0}/cik_{1}_date_{2}_extract.html'.format(DATA_DIR, proxy['cik'], proxy['date'])
     fileout=file(fn, 'w')
+
     for table in soup.find_all('table'):
+        #flag for a good table
+        is_good_table = False
         #if the first row has more than x columns, then the table might be the right table to look at
         if len(table.tr.find_all('td')) > 4:
+            #pdb.set_trace()
             tbl = [] 
-            for tr in table.find_all('tr'):
-                row = []
-                for td in tr.find_all('td'):
-                    s = removeNonAscii(td.get_text())
-                    #remove white spaces
-                    s = ' '.join(s.split())
-                    s = s.strip()
-                    row.append(s)
-                    
-                    if len(td.find_all('p'))>0:
-                        row.append('')
-                tbl.append(combine_similar_cells(row,''))
 
-                pdb.set_trace()
+            try: 
+                table.tr.td['width'] 
+                # scenario 1: the rows have "width" element, need to align the columns by width 
+                print 'table has width'
+            except:
 
-            #verify every list has same number of elements, aka. same number of columns
-            col_per_row = map(lambda x: len(x), tbl)
-            n = len(set(col_per_row))
-            if n == 1:
-                result.append(tbl)
-                fileout.write(str(table))
-                fileout.write('\n')
+                # scenario 2: the rows have no "width" element, can simply align the columns
+                print 'table has no width'
+
+                for tr in table.find_all('tr'):
+                    row = []
+                    for td in tr.find_all('td'):
+                        row.append(clean_tag_text(td.get_text()))
+                    tbl.append(row)
+                
+                # as is, without doing any work
+                if valid_table_inputs(tbl):
+                    is_good_table = True
+                else: 
+                    #positiion the columns by colspan
+                    if table.find(lambda tag: tag.name == 'td' and tag.has_attr('colspan')) is not None:
+                        tbl = rebuild_table_by_colspan(table)
+                        if valid_table_inputs(tbl):
+                            is_good_table = True
+
+                    if is_good_table == False:
+                        #remove the empty rows and try again
+                        tbl = remove_empty_rows(tbl)
+                        if valid_table_inputs(tbl):
+                            is_good_table = True
+                        else: 
+                            #assume in useful tables every cell is filled up with contents
+                            #remove all the '' cells
+                            tbl = remove_empty_cells(tbl)
+                            if valid_table_inputs(tbl):
+                                is_good_table = True
+
+                if is_good_table:
+                    result.append(tbl)
+                    fileout.write(str(table))
+                    fileout.write('\n')
     fileout.close()
 
     return result
 
-    #for table in soup.find_all('table'):
-    #    for row in table.find_all('tr'):
-    #        for cell in row.find_all('td'):
 
-
-
-def rebuild_table(orig_tbl):
-    for tbl in orig_tbl:
+def rebuild_table(orig_tbls):
+    pdb.set_trace()
+    number = 0
+    for tbl in orig_tbls:
         arr = np.array(tbl)
         # if every cell in a column is '', then remove the column from the array
         # if every cell in a row is '', then remove the row from the array
         for axis in range(2):
             idx = np.apply_along_axis(lambda a: reduce(lambda x, y: x and y, a) , axis, (arr == ''))
-            pdb.set_trace()
             if axis == 0: 
                 arr = arr[:, ~idx]
             if axis == 1: 
@@ -165,10 +268,12 @@ def rebuild_table(orig_tbl):
                     data = d_arr, #values
                     columns = header
                 )
+        df.to_csv('table{0}.csv'.format(number), sep='\t')
+        number += 1
 
 
 
-        print 'ok'
+
 
 #def insert_dynamodb(item):
 #    #http://neuralfoundry.com/scrapy-and-dynamodb-on-aws/
@@ -200,8 +305,8 @@ if __name__ == '__main__':
     
 
     index = download_index(2015, 4)
-    flower = download_proxy(index)
-    rebuild_table(flower)
+    flowers = download_proxy(index)
+    rebuild_table(flowers)
 
     #parse_file()
     
